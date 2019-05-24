fbd1a14625fc8dca422de57836180310800c98b7
tyfighting <13271508702@126.com>
2019-05-24 18:09:25 +0800

    完善订单

--- src/components/Order/Order.vue
+++ src/components/Order/Order.vue
@@ -5,9 +5,18 @@
 			<p>登录后查看外卖订单</p>
 			<router-link to="/login">立即登录</router-link>
 		</div>
-		<div>
-			<div>
-				<router-link to="/helloVue">helloVue</router-link>
+		<div v-show='!userFlag' class="order-list">
+			<div class="order-item" v-for="(v,i) in orderList" :key="i">
+					<p class="restaurant_name">
+						<span>{{v.restaurant_name}}</span>
+						<span>{{v.status_bar.title}}</span>
+					</p>
+					<p>{{v.formatted_created_at}}</p>
+					<p class="group">
+						<span>{{v.basket.group[0][0].name}}</span>
+						<span>等{{v.basket.group[0].length}}件商品</span>
+						<span>￥{{v.total_amount}}</span>
+					</p>
 			</div>
 		</div>
 		<Footer></Footer>
@@ -34,13 +43,61 @@
 		color: #fff;
 		font-size: 16px;
 	}
+	.order-item{
+		background: #fff;
+		margin-bottom: 10px;
+		padding: 5px 20px;
+	}
+	.order-item p{
+		display: flex;
+		text-align: left;
+		padding:3px 0px;
+		color: #999;
+		font-size: 10px;
+	}
+	.restaurant_name span:first-child{
+		flex: 3;
+		font-size: 16px;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		line-height: 30px;
+		color: #333;
+	}
+	.restaurant_name span:last-child{
+		flex: 2;
+		line-height: 30px;
+		color: #666;
+		text-align: right;
+	}
+	.group{
+		border-top: 1px solid #eee;
+	}
+	.group span{
+		flex: 1;
+		color: #666;
+		line-height: 30px;
+	}
+	.group span:first-child{
+		flex: 3;
+		font-size: 12px;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+	.group span:last-child{
+		flex: 1;
+		color: #333;
+		text-align: right;
+	}
 </style>
 <script>
 	import Footer from '../Common/Footer'
 	export default{
 		data(){
 			return {
-				userFlag:true
+				userFlag:true,
+				orderList:[]
 			}
 		},
 		created(){
@@ -49,6 +106,7 @@
 			}else{
 				this.userFlag=true;
 			}
+      		this.getOrder();
 		},
 		components:{
 			Footer
@@ -58,6 +116,17 @@
 				if(!localStorage.getItem("mobile")){
 					this.$router.push({path:'/login'})
 				}
+			}
+		},
+		methods:{
+			getOrder(){
+				this.$axios.get('/user/order')
+				.then(res => {
+					this.orderList = res.data;
+				})
+				.catch(err=>{
+					console.log(err)
+				})
 			}
 		}
 	}
--- src/components/helloVue.vue
+++ src/components/helloVue.vue
@@ -1,40 +1,28 @@
 <template>
   <div>
     <div class="index">mock数据</div>
-    <ul>
-      <li></li>
-    </ul>
   </div>
-</template>
- 
-<script> 
-export default {
-  data () {
-    return {
-      newsListShow: [],
-    }
-  },
-  created() {
-    this.setNewsApi();
-    this.getOrder();
-  },
-  methods:{
-    setNewsApi() {
-      this.$axios.post('/news/index', 'type=top&key=123456')
-      .then(res => {
-        console.log(res);
-        this.newsListShow = res.articles;
-      });      
+</template> 
+<script>
+  export default{
+    data () {
+      return {
+        newsListShow: [],
+      }
     },
-    getOrder(){
-      this.$axios.get('/user/order')
-      .then(res => {
-        console.log(res);
-        this.newsListShow = res.articles;
-      });
+    created() {
+      this.setNewsApi();
+    },
+    methods:{
+      setNewsApi() {
+        this.$axios.post('/news/index', 'type=top&key=123456')
+        .then(res => {
+          console.log(res);
+          this.newsListShow = res.articles;
+        });      
+      }      
     }
   }
-}
 </script>
 <style scoped>
   .index{
